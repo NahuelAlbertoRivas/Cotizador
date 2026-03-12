aba578f325c860a4085b310c2d4e64176417c88d
Nahuel Alberto Rivas <nahuelrivas41@gmail.com>
2026-03-12 00:46:05 -0300

    Ajustes

--- render-build.sh
+++ render-build.sh
@@ -9,22 +9,22 @@ cd backend
 npm install
 
 #asegurar que exista la dirección de cache para Puppeteer
-PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
-mkdir -p $PUPPETEER_CACHE_DIR
+#PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
+#mkdir -p $PUPPETEER_CACHE_DIR
 
 #instalar pup y descargar chrome
-echo ">>> Instalando Puppeteer y descargando Chrome..."
-npx puppeteer browsers intall chrome
+#echo ">>> Instalando Puppeteer y descargando Chrome..."
+#npx puppeteer browsers intall chrome
 
 #guadar/pull la cache con build cache
-if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
-    echo "...Copying Puppeteer Cache from Build Cache"
+#if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
+#    echo "...Copying Puppeteer Cache from Build Cache"
     #copiando de la dirección donde Puppeteer guarda chrome
-    cp -R /opt/render/project/src/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
-else
-    echo "...Storing Puppeteer Cache in Build Cache"
-    cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/.cache/puppeteer/chrome/
-fi
+#    cp -R /opt/render/project/src/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
+#else
+#    echo "...Storing Puppeteer Cache in Build Cache"
+#    cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/.cache/puppeteer/chrome/
+#fi
 
 cd ..
 
